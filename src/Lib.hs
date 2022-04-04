c930e3ece3401ea433c1b64c585809966c3f9d6c
cedliang <accounts@cedric.app>
2022-04-04 23:52:51 +1000

    added ada handle

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,6 +7,7 @@ import Web.Scotty
 import Network.Wai.Middleware.Cors
 import Network.HTTP.Types.Method
 import Data.Text.Lazy
+import Data.Text.Lazy.Encoding as TL.Encoding
 import Control.Monad
 import Control.Monad.IO.Class 
 import Data.Aeson 
@@ -38,6 +39,43 @@ mainScotty = scotty 3000 $ do
       InvalidHash errStr -> do 
         status $ mkStatus 404 $ B.pack "Tx not found."
         text $ errStr
+
+  addroute (Network.HTTP.Types.Method.GET) "/adahandle/:handle" $ do
+    inputHandle <- param "handle"
+    thandle <- liftAndCatchIO $ getHandle inputHandle
+    case thandle of 
+      ValidHandle handle -> Web.Scotty.text handle
+      InvalidHandle errStr -> do
+        status $ mkStatus 404 $ B.pack "Handle not found."
+        text $ errStr
+
+
+data HandleInfo
+  = ValidHandle Text
+  | InvalidHandle Text
+  deriving (Eq, Show)
+
+getHandle :: Text -> IO HandleInfo
+getHandle handleName = do
+  result <-
+    UnliftIO.Exception.try
+      ( runReq defaultHttpConfig $ do
+          response <-
+            req
+              (Network.HTTP.Req.GET)
+              (http "127.0.0.1" /: "api" /: "v1" /: "adahandle" /~ handleName)
+              NoReqBody
+              lbsResponse
+              (port 5000)
+          return response
+      )
+
+  case result of
+    Left (VanillaHttpException _) -> return $ InvalidHandle "\tNot a valid handle."
+    Right bs -> return $ ValidHandle $ TL.Encoding.decodeUtf8 $ responseBody bs
+
+
+
 
 data AddrInfo
   = ValidHash Value
