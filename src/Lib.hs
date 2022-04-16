b7b0a6f08325b0dd2d0b841745057e3f87e9ec93
cedliang <accounts@cedric.app>
2022-04-17 00:02:37 +1000

    refactoring

--- src/Lib.hs
+++ src/Lib.hs
@@ -13,7 +13,6 @@ import qualified Data.Text as T
 import qualified Data.Text.Encoding as T.Encoding
 import Data.Text.Lazy
 import Data.Text.Lazy.Encoding as TL.Encoding
-import FizzBuzz (fizzBuzz)
 import Network.HTTP.Req
 import Network.HTTP.Types (mkStatus, status400)
 import Network.HTTP.Types.Method
@@ -21,6 +20,8 @@ import Network.Wai.Middleware.Cors
 import Text.Read (readMaybe)
 import UnliftIO.Exception
 import Web.Scotty
+import GetHandle ( getHandle, HandleInfo (ValidHandle, InvalidHandle))
+import GetTx ( getTx, AddrInfo (ValidHash, InvalidHash))
 
 mainScotty :: IO ()
 mainScotty = scotty 3000 $ do
@@ -28,11 +29,6 @@ mainScotty = scotty 3000 $ do
 
   addroute Network.HTTP.Types.Method.GET "/helloworld" $ do
     html "Hello, world!"
-
-  addroute Network.HTTP.Types.Method.GET "/printargs/:arg1/:arg2" $ do
-    arg1 <- param "arg1"
-    arg2 <- param "arg2"
-    html $ arg1 <> fromStrict " " <> arg2
 
   addroute Network.HTTP.Types.Method.GET "/tx/:txhash" $ do
     inputHash <- param "txhash"
@@ -52,56 +48,3 @@ mainScotty = scotty 3000 $ do
         status $ mkStatus 404 $ B.pack "Handle not found."
         text errStr
 
-  addroute Network.HTTP.Types.Method.GET "/fizzbuzz/:arg" $ do
-    arg <- param "arg"
-    let n = readMaybe $ T.unpack arg :: Maybe Int
-    case n of
-      Just val -> Web.Scotty.text $ fromStrict $ T.unlines $ fizzBuzz val
-      _ -> do
-        status status400
-        text "Not a valid n"
-
-data HandleInfo
-  = ValidHandle Text
-  | InvalidHandle Text
-  deriving (Eq, Show)
-
-getHandle :: Text -> IO HandleInfo
-getHandle handleName = do
-  result <-
-    UnliftIO.Exception.try
-      ( runReq defaultHttpConfig $ do
-          req
-            Network.HTTP.Req.GET
-            (http "127.0.0.1" /: "api" /: "v1" /: "adahandle" /~ handleName)
-            NoReqBody
-            lbsResponse
-            (port 5000)
-      )
-
-  case result of
-    Left (VanillaHttpException _) -> return $ InvalidHandle "\tNot a valid handle."
-    Right bs -> return $ ValidHandle $ TL.Encoding.decodeUtf8 $ responseBody bs
-
-data AddrInfo
-  = ValidHash Value
-  | InvalidHash Text
-  deriving (Eq, Show)
-
-getTx :: Text -> IO AddrInfo
-getTx addr = do
-  result <-
-    UnliftIO.Exception.try
-      ( runReq defaultHttpConfig $ do
-          req
-            Network.HTTP.Req.GET
-            (http "127.0.0.1" /: "api" /: "v1" /: "tx" /~ addr)
-            NoReqBody
-            jsonResponse
-            (port 5000)
-      ) ::
-      (FromJSON j) => IO (Either HttpException (JsonResponse j))
-
-  case result of
-    Left (VanillaHttpException _) -> return $ InvalidHash "\tNot a valid txhash."
-    Right v -> return $ ValidHash (responseBody v :: Value)
