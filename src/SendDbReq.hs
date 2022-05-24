f9fd6067b87ce7741feab62db24f3e353823636e
cedliang <accounts@cedric.app>
2022-05-24 20:23:32 +1000

    better formatting

--- src/SendDbReq.hs
+++ src/SendDbReq.hs
@@ -3,11 +3,11 @@
 
 module SendDbReq (sendReq, QueryServerConfig (QueryServerConfig)) where
 
-import           Control.Exception          (try)
+import           Control.Exception      (try)
 import           Control.Monad.IO.Class
 import           Control.Monad.Reader
 import           Data.Aeson
-import qualified Data.Text.Lazy             as TL
+import qualified Data.Text.Lazy         as TL
 import           Network.HTTP.Req
 import           Servant
 
@@ -28,7 +28,23 @@ sendReq queryScheme searchTable = do
   either (const $ lift $ throwError $ err500 {errBody = "Internal server error"}) (pure . responseBody) result
   where
     httpReq :: Url 'Http -> Option 'Http -> IO (Either HttpException (JsonResponse Value))
-    httpReq uri portScheme = try $ runReq defaultHttpConfig $ req Network.HTTP.Req.GET (uri /~ searchTable) NoReqBody jsonResponse (fst queryScheme <> portScheme)
+    httpReq uri portScheme =
+      try $
+        runReq defaultHttpConfig $
+          req
+            Network.HTTP.Req.GET
+            (uri /~ searchTable)
+            NoReqBody
+            jsonResponse
+            (fst queryScheme <> portScheme)
 
     httpsReq :: Url 'Https -> Option 'Https -> IO (Either HttpException (JsonResponse Value))
-    httpsReq uri portScheme = try $ runReq defaultHttpConfig $ req Network.HTTP.Req.GET (uri /~ searchTable) NoReqBody jsonResponse (snd queryScheme <> portScheme)
+    httpsReq uri portScheme =
+      try $
+        runReq defaultHttpConfig $
+          req
+            Network.HTTP.Req.GET
+            (uri /~ searchTable)
+            NoReqBody
+            jsonResponse
+            (snd queryScheme <> portScheme)
