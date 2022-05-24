cee03fdc94fdc58c20cb318764228624803ef3be
cedliang <accounts@cedric.app>
2022-05-25 00:02:23 +1000

    simplification by eliminating dependency on req

--- src/GetHandle.hs
+++ src/GetHandle.hs
@@ -6,13 +6,13 @@ module GetHandle (handleHandler) where
 import           Control.Monad
 import           Control.Monad.Reader
 import           Data.Aeson
-import qualified Data.ByteString            as B
-import qualified Data.Text.Lazy             as TL
-import qualified Data.Text.Lazy.Encoding    as TL
-import           Network.HTTP.Req
-import           SendDbReq
+import qualified Data.ByteString         as B
+import qualified Data.Text.Lazy          as TL
+import qualified Data.Text.Lazy.Encoding as TL
+import           Network.HTTP.Simple
 import           Servant
 import           Text.Hex
+import           Types
 
 newtype RawHandleAddr = Address {getAddr :: TL.Text}
   deriving (Show)
@@ -20,27 +20,27 @@ newtype RawHandleAddr = Address {getAddr :: TL.Text}
 instance FromJSON RawHandleAddr where
   parseJSON = withObject "RawhandleAddr" $ fmap Address . (.: "address") <=< (.: "tx_out")
 
-optionScheme :: TL.Text -> (Option 'Http, Option 'Https)
-optionScheme hexHandleName =
-  ( "select" =: ("id,tx_out!inner(id,address),multi_asset!inner(id,policy,name)" :: TL.Text)
-      <> "order" =: ("id.desc" :: TL.Text)
-      <> "limit" =: (1 :: Int)
-      <> "multi_asset.policy" =: ("eq.\\xf0ff48bbb7bbe9d59a40f1ce90e9e9d0ff5002ec48f232b49ca0fb9a" :: TL.Text)
-      <> "multi_asset.name" =: ("eq.\\x" <> hexHandleName :: TL.Text),
-    "select" =: ("id,tx_out!inner(id,address),multi_asset!inner(id,policy,name)" :: TL.Text)
-      <> "order" =: ("id.desc" :: TL.Text)
-      <> "limit" =: (1 :: Int)
-      <> "multi_asset.policy" =: ("eq.\\xf0ff48bbb7bbe9d59a40f1ce90e9e9d0ff5002ec48f232b49ca0fb9a" :: TL.Text)
-      <> "multi_asset.name" =: ("eq.\\x" <> hexHandleName :: TL.Text)
-  )
+query :: TL.Text -> Query
+query hexHandleName =
+  [ ("select", Just "id,tx_out!inner(id,address),multi_asset!inner(id,policy,name)"),
+    ("order", Just "id.desc"),
+    ("limit", Just "1"),
+    ("multi_asset.policy", Just "eq.\\xf0ff48bbb7bbe9d59a40f1ce90e9e9d0ff5002ec48f232b49ca0fb9a"),
+    ("multi_asset.name", Just $ "eq.\\x" <> B.toStrict (TL.encodeUtf8 hexHandleName))
+  ]
 
-searchTable :: TL.Text
-searchTable = "ma_tx_out"
-
-handleHandler :: TL.Text -> ReaderT QueryServerConfig Handler TL.Text
+handleHandler :: TL.Text -> ReaderT Config Handler TL.Text
 handleHandler inputHandle = do
-  thandle <- sendReq (optionScheme $ removeDollar inputHandle) searchTable
-  case fromJSON thandle :: Result [RawHandleAddr] of
+  cfg <- ask
+  request' <- parseRequest $ serverHost cfg ++ "/ma_tx_out"
+  let request = setRequestQueryString (query $ removeDollar inputHandle)
+              $ setRequestSecure (https cfg)
+              $ request'
+      portSetRequest = case serverPort cfg of
+        Nothing -> request
+        Just p  -> setRequestPort p request
+  response <- httpJSON portSetRequest
+  case fromJSON $ getResponseBody response :: Result [RawHandleAddr] of
     Error _         -> throwError $ err500 {errBody = "Internal server error"}
     Success []      -> throwError $ err404 {errBody = "Handle not found"}
     Success (x : _) -> pure $ getAddr x
--- src/Server.hs
+++ src/Server.hs
@@ -1,58 +1,33 @@
 {-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeFamilies      #-}
 {-# LANGUAGE TypeOperators     #-}
 
 module Server (servantIO) where
 
-import qualified Control.Exception
-import           Control.Monad
 import           Control.Monad.Reader
-import           Data.Maybe
 import qualified Data.Text                            as T
 import qualified Data.Text.Lazy                       as TL
 import           GetHandle
-import           Network.HTTP.Req
 import           Network.Wai.Handler.Warp
 import           Network.Wai.Middleware.Cors
 import           Network.Wai.Middleware.RequestLogger
-import           SendDbReq
 import           Servant
-import           System.Exit
-import           Text.URI                             as TU
-
-data Config = Config
-  { hostPort   :: Int,
-    serverUrl  :: T.Text,
-    serverPort :: Maybe Int
-  }
-  deriving (Show, Eq)
+import           Types
 
 defaultConfig :: Config
-defaultConfig = Config {hostPort = 4000, serverUrl = "https://cedric.app/api/dbsync/postgrest", serverPort = Nothing}
+defaultConfig = Config {hostPort = 4000, https = True, serverHost = "https://cedric.app/api/dbsync/postgrest", serverPort = Nothing}
 
 -- defaultConfig :: Config
--- defaultConfig = Config {hostPort = 4000, serverUrl = "http://192.168.20.10/", serverPort = Just 3000}
-
-getUrlFromConfig :: IO QueryServerConfig
-getUrlFromConfig = do
-  serverUri <- Control.Exception.catch (mkURI $ serverUrl defaultConfig) (const $ die "Invalid URL cannot be parsed" :: ParseException -> IO TU.URI)
-  let serverP = serverPort defaultConfig
-      rawServerUrl = useURI serverUri
-  when (isNothing rawServerUrl) (die "Invalid URL cannot be parsed")
-  case fromJust rawServerUrl of
-    Left a  -> pure $ QueryServerConfig (Left $ fst a) serverP
-    Right a -> pure $ QueryServerConfig (Right $ fst a) serverP
+-- defaultConfig = Config {hostPort = 4000, https = False, serverHost = "http://192.168.20.10", serverPort = Just 3000}
 
 servantIO :: IO ()
 servantIO = do
-  queryUrl <- getUrlFromConfig
-  run (hostPort defaultConfig) $ (simpleCors . logStdoutDev) $ servApp queryUrl
+  run (hostPort defaultConfig) $ (simpleCors . logStdoutDev) $ servApp defaultConfig
 
-servApp :: QueryServerConfig -> Application
+servApp :: Config -> Application
 servApp = serve (Proxy :: Proxy CardanoAPI) . server1
 
 type CardanoAPI = "adahandle" :> Capture "inputHandle" TL.Text :> Get '[PlainText] TL.Text
 
-server1 :: QueryServerConfig -> ServerT CardanoAPI Handler
+server1 :: Config -> ServerT CardanoAPI Handler
 server1 queryUrl = flip runReaderT queryUrl . handleHandler
